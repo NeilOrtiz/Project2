2637010614a9c7236c8f2f88dc8fbd15fd9316f0
nao160030 <nao160030@utdallas.edu>
2018-03-24 02:56:20 -0500

    Add menu using if statements

--- src/SysFile/Parent.java
+++ src/SysFile/Parent.java
@@ -1,7 +1,11 @@
 package SysFile;
 
+import java.util.Scanner;
+
 public class Parent {
+
     public static void main (String[] args) {
+
         if (args.length!=2) {
 			System.err.println("Usage: java -jar Project2/dist/Parent.jar <ID> <M|s|c>");
 			System.exit(1);
@@ -19,7 +23,50 @@ public class Parent {
 
             System.out.println("This is the client "+args[0]);
 
+            System.out.println("Enter one of the following commands:");
+            System.out.println("1 - Create");
+            System.out.println("2 - Read");
+            System.out.println("3 - Append");
+            System.out.println("4 - Exit");
+            Scanner choice = new Scanner(System.in);
+            System.out.println();
+            System.out.println("Enter \"1\", \"2\", \"3\"or \"4\"");
+            System.out.println();
+            int choiceEntry=choice.nextInt();
+
+            while (choiceEntry!=4) {
+
+                if (choiceEntry<1 || choiceEntry >3) {
+
+                    System.out.println("Enter \"1\", \"2\", \"3\"or \"4\"");
+                    choiceEntry=choice.nextInt();
+
+                }
+                else if (choiceEntry==1) {
+                    System.out.println("Creating");
+                    System.out.println();
+                }
+                else if (choiceEntry==2) {
+                    System.out.println("Reading");
+                    System.out.println();
+                }
+                else if (choiceEntry==3){
+                    System.out.println("Appending");
+                    System.out.println();
+                }
+                else if (choiceEntry==4){
+                    System.out.println("Saliendo");
+                    System.exit(1);
+                }
+
+                System.out.println("Enter \"1\", \"2\", \"3\"or \"4\"");
+                choiceEntry=choice.nextInt(); 
+            }
+
+            
+
         } else {
+
             System.err.println("Usage: java -jar Project2/dist/Parent.jar <ID> <M|s|c>");
 			System.exit(1);
         }
