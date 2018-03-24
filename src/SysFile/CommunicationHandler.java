75d469f5c86f3c7be4c628b194afac39e2db0027
nao160030 <nao160030@utdallas.edu>
2018-03-24 16:40:04 -0500

    Creation CommunicationHandler class and empty estComm method

---
+++ src/SysFile/CommunicationHandler.java
@@ -0,0 +1,19 @@
+package SysFile;
+
+public class CommunicationHandler {
+    private Parent dad;
+    private int myID;
+
+    public CommunicationHandler(Parent dad, int myID) {
+        this.dad=dad;
+        this.myID=myID;
+    }
+
+    public boolean estComm(String typeHost) {
+        boolean success=false;
+        //TODO: CH.estComm()
+
+        return success;
+
+    }
+}
