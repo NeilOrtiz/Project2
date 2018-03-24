67acb762f47339d34f6bfd1c852ff055c7bf93ef
nao160030 <nao160030@utdallas.edu>
2018-03-24 18:51:39 -0500

    Creation Empty Receiver.java 2

---
+++ src/SysFile/Receiver.java
@@ -0,0 +1,24 @@
+package SysFile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Receiver extends Thread {
+    private BufferedReader readSocket;
+    private Parent dad;
+    private CommunicationHandler cH;
+
+    public Receiver(BufferedReader readSocket,Parent dad,CommunicationHandler cH) {
+        super("Receiver");
+        this.dad=dad;
+        this.cH=cH;
+        this.readSocket=readSocket;
+    }
+
+    @Override
+	public void run() {
+        //TODO: Receiver.run()
+
+    }
+
+}
