64071fa1267f6d5b439bd49ecbad25365f3ba700
nao160030 <nao160030@utdallas.edu>
2018-03-24 00:37:27 -0500

    Add the lines arguments M, s, c

--- src/SysFile/Parent.java
+++ src/SysFile/Parent.java
@@ -2,6 +2,27 @@ package SysFile;
 
 public class Parent {
     public static void main (String[] args) {
-        System.out.println("Hello World5");
+        if (args.length!=2) {
+			System.err.println("Usage: java -jar Project2/dist/Parent.jar <ID> <M|s|c>");
+			System.exit(1);
+        }
+        
+        if (args[1].equals("M")) {
+
+            System.out.println("This is the M-server");
+
+        } else if (args[1].equals("s")) {
+
+            System.out.println("This is the file-server "+args[0]);
+
+        } else if (args[1].equals("c")) {
+
+            System.out.println("This is the client "+args[0]);
+
+        } else {
+            System.err.println("Usage: java -jar Project2/dist/Parent.jar <ID> <M|s|c>");
+			System.exit(1);
+        }
+
     }
 } 
