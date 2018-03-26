0d962e28b15a2f7c7a6a87b8c08a4b1026abfe24
nao160030 <nao160030@utdallas.edu>
2018-03-26 06:55:25 -0500

    Chunk.java delete some unused libraries

--- src/SysFile/Chunk.java
+++ src/SysFile/Chunk.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.zip.CRC32;
-import javax.swing.*;
+//import java.util.zip.CRC32;
+//import javax.swing.*;
 
 public class Chunk {
 
