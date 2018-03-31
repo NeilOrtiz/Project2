f627a2e2708ed8475e352b7a9d5be52925492c3e
nao160030 <nao160030@utdallas.edu>
2018-03-31 03:24:41 -0500

    [add] basic empty funtions Fserver.java

--- src/SysFile/Fserver.java
+++ src/SysFile/Fserver.java
@@ -5,17 +5,30 @@ public class Fserver {
 
     private Parent dad;
     private CommunicationHandler cH;
+    private Chunk chunk;
 
     public Fserver(Parent dad, CommunicationHandler cH) {
 
         this.dad=dad;
         this.cH=cH;
+        this.chunk=new Chunk();
     }
 
     public void execute(){
-
+        //HearBeat
         HeartBeat heart = new HeartBeat(dad.myID,dad,cH,dad.folder);
         heart.start();
+    }
 
+    public void appendChunk() {
+        //TODO: Fserver.append()
+    }
+
+    public void newChunk(){
+        //TODO: Fserver.newChunk()
+    }
+
+    public void readChunk(){
+        //TODO: Fserver.readChunk()
     }
 }
